ff5d7dfeda395f235bdb25e09fd573733167cc82
billryan <yuanbin2014@gmail.com>
2019-03-18 23:05:57 +0800

    add TypesafeConfig

---
+++ src/main/java/me/yuanbin/common/config/TypesafeConfig.java
@@ -0,0 +1,33 @@
+package me.yuanbin.common.config;
+
+import com.google.common.base.Strings;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+public class TypesafeConfig {
+    private static final String PROFILES = "APP_PROFILES_ACTIVE";
+    private static final String DEFAULT_PROFILES = "dev";
+
+    private static String getConfigFile(String env) {
+        String configEnv = env;
+        if (Strings.isNullOrEmpty(env)) {
+            String envVar = System.getenv(PROFILES);
+            String propVar = System.getProperty(PROFILES);
+            // read from -D first, then OS environment.
+            // Set with default profiles if env and prop is empty.
+            configEnv = Strings.isNullOrEmpty(propVar) ? envVar : propVar;
+        }
+
+        if (Strings.isNullOrEmpty(configEnv)) configEnv = DEFAULT_PROFILES;
+        return String.format("application-%s.conf", configEnv);
+    }
+
+    public static Config build() {
+        return build(null);
+    }
+
+    public static Config build(String env) {
+        String configFile = getConfigFile(env);
+        return ConfigFactory.load(configFile);
+    }
+}
---
+++ src/test/java/me/yuanbin/common/config/TypesafeConfigTest.java
@@ -0,0 +1,17 @@
+package me.yuanbin.common.config;
+
+import com.typesafe.config.Config;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TypesafeConfigTest {
+    private Config config = TypesafeConfig.build("ci");
+
+    @Test
+    public void testConfig() {
+        assertEquals("ci.yuanbin.me", config.getString("common.host"));
+        assertEquals(8080, config.getInt("common.port"));
+        assertEquals("ci", config.getString("app.profiles.active"));
+    }
+}
